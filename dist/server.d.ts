68b1188f766f51da5e8bfff31ff4b418de558abe
David Cabal <dncabal@gmail.com>
2025-10-01 14:54:11 -0500

    Remove dist/ and node_modules/ from git tracking

--- dist/server.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/**
- * MCP Server for Kalendis API Client Generation
- * Provides tools to generate TypeScript clients with proper x-api-key authentication
- */
-export {};
-//# sourceMappingURL=server.d.ts.map
